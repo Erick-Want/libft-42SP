346b514014c564f5773ab175f5b01e083e3bfd61
Erick Matheus <erick_linux@Erick-Matheus.localdomain>
2021-08-02 22:43:13 -0300

    Primeiro Envio

---
+++ ft_bzero.c
@@ -0,0 +1,13 @@
+void bzero(void *s, size_t n)
+{
+	size_t i;
+	char *s1;
+
+	i = 0;
+	s1 = s;
+	while(n > i)
+	{
+		s1[i] = '\0';
+		i++;
+	}
+}
---
+++ ft_isdigit.c
@@ -0,0 +1,6 @@
+int isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return(1);
+	return(0);
+}
