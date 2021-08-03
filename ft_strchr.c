8f5fb2cba0fa6cc624b5c8a8320d8f6af6385bed
Erick Matheus <erick_linux@Erick-Matheus.localdomain>
2021-08-02 23:24:21 -0300

    segundo envio do dia - sad

---
+++ ft_strchr.c
@@ -0,0 +1,27 @@
+char *strchr(const char *s, int c)
+{
+	size_t i;
+
+	i = 0;
+	while(s[i] =! '\0')
+	{
+		if (s[i] == c)
+			return(&s[i]);
+		i++;
+	}
+	if (s[i] == '\0' && c == '\0')
+		return(&s[i]);
+	return(NULL);
+}
+{
+	size_t i;
+
+	i = 0;
+	while(s[i] =! c)
+		i++;
+	if (s[i] == c)
+		return(&s[i]);
+	if (s[i] == '\0' && c == '\0')
+		return(&s[i]);
+	return(NULL);
+}
