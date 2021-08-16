821b535669adb7e0a83ee778eca094dd34717cea
Erick Matheus <erick_linux@Erick-Matheus.localdomain>
2021-08-16 15:16:31 -0300

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ermatheu <ermatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:12:14 by ermatheu          #+#    #+#             */
-/*   Updated: 2021/08/16 15:14:02 by ermatheu         ###   ########.fr       */
+/*   Updated: 2021/08/16 15:16:15 by ermatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*newstr;
-	size_t	i;
-	size_t	i_beggin;
+	size_t	i_beg;
 	size_t	i_end;
 
 	if (!s1 || !set)
 		return (NULL);
-	i_beggin = 0;
+	i_beg = 0;
 	i_end = ft_strlen(s1);
-	while (s1[i_beggin] && ft_strchr(set, s1[i_beggin]))
-		i_beggin++;
+	if (i_end == 0)
+		return (ft_calloc(1, sizeof(char)));
+	while (s1[i_beg] && ft_strchr(set, s1[i_beg]))
+		i_beg++;
 	while (i_end && ft_strchr(set, s1[i_end]))
 		i_end--;
-	if (i_end == i_beggin)
-		return (ft_calloc(1, sizeof(char)));
-	newstr = malloc(sizeof(char) * (i_end - i_beggin + 2));
-	if (newstr == NULL)
-		return (NULL);
-	i = 0;
-	while (i <= i_end - i_beggin)
-	{
-		newstr[i] = s1[i_beggin + i];
-		i++;
-	}
-	newstr[i] = '\0';
-	return (newstr);
+	return (ft_substr(s1, i_beg, (i_end - i_beg + 1)));
 }
-
-
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	size_t	i_beg;
-// 	size_t	i_end;
-// 	// size_t	i;
-// 	// char	*newstr;
-
-// 	if (s1 == NULL || !set)
-// 		return (NULL);
-// 	i_beg = 0;
-// 	i_end = ft_strlen(s1);
-// 	if (i_end == 0)
-// 		return (ft_calloc(1, sizeof(char)));
-// 	while (s1[i_beg] && ft_strchr(set, s1[i_beg]))//"-aErick-Matheus-a"
-// 		i_beg++;
-// 	while (i_end && ft_strchr(set, s1[i_end]))
-// 		i_end--;
-// 	return (ft_substr(s1, i_beg, (i_end - i_beg + 1)));
-// }
 
 // int main()
 // {
