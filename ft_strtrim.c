74967fbe9d78844bbf234e7b8663d8d6f6928c44
Erick Matheus <erick_linux@Erick-Matheus.localdomain>
2021-08-16 15:08:51 -0300

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ermatheu <ermatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:12:14 by ermatheu          #+#    #+#             */
-/*   Updated: 2021/08/16 15:06:46 by ermatheu         ###   ########.fr       */
+/*   Updated: 2021/08/16 15:08:48 by ermatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		i_beg++;
 	while (i_end && ft_strchr(set, s1[i_end]))
 		i_end--;
-	return (ft_substr(s1, i_beg, (i_end - i_beg + 1)));
+	return (ft_substr(&s1[i_beg], 0, (i_end - i_beg + 1)));
 }
 
 // int main()
