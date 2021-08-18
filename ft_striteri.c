ba358144b1e44c3195c29e7dff8b3e749fdb6f90
Erick Matheus <erick_linux@Erick-Matheus.localdomain>
2021-08-18 13:34:37 -0300

    update

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ermatheu <ermatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 15:00:04 by ermatheu          #+#    #+#             */
-/*   Updated: 2021/08/05 15:07:20 by ermatheu         ###   ########.fr       */
+/*   Updated: 2021/08/18 13:33:43 by ermatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
+	if (!s)
+		return ;
 	while (s[i] != '\0')
 	{
 		f(i, s + i);
